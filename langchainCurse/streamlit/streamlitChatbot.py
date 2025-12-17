cdf90510e6073b8bde760428fb23bb95907ccff4
JuanAndres <juan.tuberquia@infracommerce.lat>
2025-12-16 21:22:33 -0500

    add selector model

--- langchainCurse/streamlit/streamlitChatbot.py
+++ langchainCurse/streamlit/streamlitChatbot.py
@@ -1,12 +1,33 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_core.prompts import PromptTemplate
 import streamlit as st
 
-chatModel = ChatOpenAI(model="gpt-4o-mini", temperature=0.5)
+# chatModel = ChatOpenAI(model="gpt-4o-mini", temperature=0.5)
 
 st.set_page_config(page_title="ChatBot basico", page_icon="🤖")
 st.title("Chat Faster")
 st.markdown("Bienvenido al chatBot basico usando Langchain y Streamlit")
+
+temperature = ""
+model_name = ""
+
+with st.sidebar:
+    st.title("Configuración")
+    temperature = st.slider("Temperatura", 0.0, 1.0, 0.5, 0.1)
+    model_name = st.selectbox("Modelo", ["gpt-3.5-turbo", "gpt-4", "gpt-4o-mini"])
+    chatModel = ChatOpenAI(model=model_name, temperature=temperature)
+
+template = PromptTemplate(
+  input_variables=["mensaje", "historial"],
+  template=(
+    "Eres un asistente útil y amigable llamado ChatBot fast \n\n"  
+    "historial de conversacion: {historial} \n\n"
+    "Responde de manera clara y concisa a la siguiente pregunta: {mensaje} \n\n"
+ ) 
+)
+
+chain = template | chatModel
 
 # initial message history
 if "messages" not in st.session_state:
@@ -21,7 +42,7 @@ for msg in st.session_state.messages:
     with st.chat_message(role):
         st.markdown(msg.content)
 
-# # question user
+# question user
 question = st.chat_input("Escribe tu pregunta")
 if question:
     with st.chat_message("human"):
@@ -31,7 +52,10 @@ if question:
     st.session_state.messages.append(HumanMessage(content=question))
 
     # response from assistant
-    response = chatModel.invoke(st.session_state.messages)
+    response = chain.invoke({
+      "mensaje": question,
+      "historial": st.session_state.messages
+    })
 
     #show response in the interface
     with st.chat_message("assistant"):
