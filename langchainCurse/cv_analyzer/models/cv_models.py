1105ef110baf4f431c20fdfe79c86092bdd57df3
JuanAndres <juan.tuberquia@infracommerce.lat>
2026-01-09 19:20:28 -0500

    add models, prompts, and pdf processor

---
+++ langchainCurse/cv_analyzer/models/cv_models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+
+class CV_analyzer(BaseModel):
+  name: str = Field(description="Nombre del candidato a la vacante")
+  time_experience: int = Field(description="Años de experiencia del candidato")
+  strengths: list[str] = Field(description="Listado de fortalezas del candidato, 3-4 fortalezas")
+  education: str = Field(description="Nivel educativo mas alto del candidato")
+  experience_relevant: str = Field(description="Resumen corto de la experiencia mas relevante")
+  weaknesses: list[str]= Field(description="Listado de oportunidades de mejora del candidado, 3-4 mejoras")
+  percentaje: int = Field(description="Porcentaje de ajuste del puesto (0-100) en la vacante")
---
+++ langchainCurse/cv_analyzer/service/pdf_processor.py
@@ -0,0 +1,22 @@
+import pypdf
+
+def readPdf(filePdf):
+  print("Leyendo pdf...", filePdf)
+  try:
+    pdf = pypdf.PdfReader(filePdf)
+    completText
+
+    for page in pdf.pages:
+      textPage = page.extract_text()
+      if textPage.strip():
+        print("este es el texto de la pagina:", textPage)
+        completText += textPage
+    completText = completText.strip()
+
+    if not completText:
+      return "Error: El pdf esta vacio o no tiene imagenes"
+    
+    return completText
+    
+  except Exception as e:
+    return "Error: No se pudo leer el pdf: {e}"
