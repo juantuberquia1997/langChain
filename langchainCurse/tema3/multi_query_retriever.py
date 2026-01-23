5bbd503a71727e561966a5a96fb4b79e841d23a2
JuanAndres <juan.tuberquia@infracommerce.lat>
2026-01-22 19:31:46 -0500

    add multiquery retriever

---
+++ langchainCurse/tema3/multi_query_retriever.py
@@ -0,0 +1,23 @@
+# uso de multi query retriever para  hacer una consulta mas avanzada de la informacion en una base de datos vectorial
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_openai import ChatOpenAI
+from langchain_classic.retrievers.multi_query import MultiQueryRetriever
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+vector_store = Chroma(
+  embedding_function = OpenAIEmbeddings(model="text-embedding-3-large"), 
+  persist_directory= "C:\\Users\\1234\\Documents\\github\\curseIA\\langchainCurse\\tema3\\chromadb" 
+)
+
+base_retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 2})
+retriever = MultiQueryRetriever.from_llm(llm=llm, retriever=base_retriever)
+
+question = "¿cual es la copa mas importante que ha ganado Atletico nacional?" 
+result = retriever.invoke(question)
+
+print("result__", result)
+
+for i, doc in enumerate(result):
+    print(f"similarity_data: {doc.page_content}")
