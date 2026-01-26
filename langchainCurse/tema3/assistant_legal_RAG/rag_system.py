8b8dddf65b24b0e3e3a5e5645848a020cf5477a3
JuanAndres <juan.tuberquia@infracommerce.lat>
2026-01-26 16:09:50 -0500

    add dependencies to rag system

---
+++ langchainCurse/tema3/assistant_legal_RAG/rag_system.py
@@ -0,0 +1,9 @@
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_classic.retrievers.multi_query import MultiQueryRetriever
+# from langchain.retrievers import EnsembleRetriever
+import streamlit as st
+
